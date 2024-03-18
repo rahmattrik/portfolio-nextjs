baf9e5e903e843ab9001518594ee47efa96e5875
Rahmattrik <rahmattrik@gmail.com>
2024-03-18 07:14:14 +0700

    Update UI

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,10 +1,20 @@
+import Image from "next/image";
 import React from "react";
+
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+    <footer className="footer border z-10 border-t-[#33353F] text-white">
       <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
+        <span>
+        <Image
+                className="relative px-0"
+                src={"/images/coding.png"}
+                alt="logo"
+                width={32}
+                height={16}
+              />
+        </span>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -14,18 +14,18 @@ const HeroSection = () => {
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className="col-span-8 place-self-center text-center sm:text-left justify-self-start"
+          className="col-span-7 place-self-center text-center sm:text-left justify-self-start"
         >
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+          <h1 className="ttext-white text-4xl sm:text-5xl lg:text-6xl font-extrabold mb-6">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-blue-400 to-blue-700">
               Hello, I&apos;m{" "}
             </span>
             <br></br>
             <TypeAnimation
               sequence={[
-                "Rahmat Tri K",
+                "Rahmat Tri Kuncoro",
                 1000,
-                "Web Developer",
+                "Full Stack Developer",
                 1000,
               ]}
               wrapper="span"
@@ -33,24 +33,22 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-1 lg:text-xl text-justify">
-          Crafting Digital Experiences from Front to Back:
-          </p>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl text-justify">
-          Your Full Stack Developer Journey Begins Here.
+          <p className="text-[#ADB7BE] text-lg mb-6 pr-5 text-between lg:text-xl">
+              Crafting Digital Experiences from Front to Back: Your Full Stack Developer Journey Begins Here.
+              Explore, Innovate, Create, Collaborate, Excel Together.
           </p>
           <div>
             <Link
               href="/#contact"
-              className="px-6 inline-block py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 to-blue-600 hover:bg-slate-200 text-white"
+              className="px-6 inline-block py-3 w-full sm:w-fit rounded-lg mr-4 bg-gradient-to-br from-blue-500 to-blue-600 hover:bg-slate-200 text-white"
             >
               Contact Me
             </Link>
             <Link
               href="/"
-              className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-gray-600 to-gray-800 hover:bg-slate-800 text-white mt-3"
+              className="px-1 inline-block py-1 w-full sm:w-fit rounded-lg bg-gradient-to-br from-gray-600 to-gray-800 hover:bg-slate-800 text-white mt-3"
             >
-              <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
+              <span className="block bg-[#121212] hover:bg-slate-800 rounded-lg px-5 py-2">
                 Download CV
               </span>
             </Link>
@@ -62,11 +60,11 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#181818] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full mt-4 bg-gradient-to-r from-blue-500 to-blue-700 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
-              src="/images/3d-man.jpg"
+              src="/images/character-section.png"
               alt="hero image"
-              className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2 rounded-full"
+              className="absolute transform -translate-x-1/2 -translate-y-1/3 top-1/2 left-1/2"
               width={350}
               height={350}
             />
